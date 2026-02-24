a0472dc4ee1272817ccd97fe628a3da37ec32ece
GustavoLisboa0 <gustavo20031010@gmail.com>
2026-02-24 19:51:01 -0300

    feat: conclusão part1

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+CAPÍTULO 2 - DESAFIOS 1 A 5
+Bruno Carraro e Gustavo Lisboa
+*/
 
-export default function App() {
+// Importando biblioteca
+import React from "react";
+
+// Importando componentes react-native
+import {View, Text} from 'react-native';
+
+// Função principal - Manipulação de Views
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{backgroundColor: '#37c5e9', height: '100%', width: '300px'}}>
+      
+      <View style={{backgroundColor: '#d137e9', height: '20%', width: '100%' }}>
+        <Text>COMPONENTE VIEW</Text>
+        <Text>Bruno Carraro e Gustavo Lisboa</Text>
+        <Text>dd/mm/aaaa</Text>
+      </View>
+
+      <View style={{backgroundColor: '#99e937', height: '70%', width: '100px'}}>
+        <Text>COMPONENTE VIEW</Text>
+        <Text>Bruno Carraro e Gustavo Lisboa</Text>
+        <Text>dd/mm/aaaa</Text>
+      </View>
+
+      <View style={{backgroundColor: '#e98737', height: '10%', width: '200px'}}>
+        <Text>COMPONENTE VIEW</Text>
+        <Text>Bruno Carraro e Gustavo Lisboa</Text>
+        <Text>dd/mm/aaaa</Text>
+      </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
