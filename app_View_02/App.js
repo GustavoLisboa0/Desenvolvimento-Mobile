c8662c13658803a1483ffea86e69c417f2d732d8
GustavoLisboa0 <gustavo20031010@gmail.com>
2026-03-01 17:30:11 -0300

    Atualiza layout das views em app_View_02 e app_View_03

--- app_View_02/App.js
+++ app_View_02/App.js
@@ -12,7 +12,20 @@ import {View, Text} from 'react-native';
 // Função principal - Manipulação de Views
 function App() {
   return (
-    <View>
+    <View style={{backgroundColor: '#61635c', height: '100%', width: '100%', paddingTop: 40, paddingLeft: 20}}>
+
+      <View style={{backgroundColor: '#a2e004',width: 300, height: 100, borderWidth: 4, borderColor: 'black', borderTopLeftRadius: 30, borderTopRightRadius: 30, marginBottom: 20, padding: 10}}>
+        <Text style={{ fontSize: 18 }}>COMPONENTE VIEW</Text>
+      </View>
+
+      <View style={{backgroundColor: '#ff6600', width: 300, height: 300, borderWidth: 4, borderColor: 'black', marginBottom: 20, padding: 10}}>
+        <Text style={{ fontSize: 22 }}>CONTEÚDO</Text>
+      </View>
+
+      <View style={{backgroundColor: '#0073d1', width: 300, height: 100, borderWidth: 4, borderColor: 'black', borderBottomLeftRadius: 30, borderBottomRightRadius: 30, padding: 10}}>
+        <Text>Bruno e Gustavo</Text>
+        <Text>dd/mm/aaa</Text>
+      </View>
 
     </View>
   );
--- app_View_03/App.js
+++ app_View_03/App.js
@@ -12,7 +12,13 @@ import {View, Text} from 'react-native';
 // Função principal - Manipulação de Views
 function App() {
   return (
-    <View>
+    <View style={{flex: 1, marginTop: 50, padding: 50, backgroundColor: '#5f5d5d', flexDirection: 'row'}}>
+
+      <View style={{width: 300, height: 300, backgroundColor: '#488a1d'}}></View>
+
+      <View style={{width: 300, height: 300, backgroundColor: '#ff5550'}}></View>
+
+      <View style={{width: 300, height: 300, backgroundColor: '#e2cd0e'}}></View>
 
     </View>
   );
