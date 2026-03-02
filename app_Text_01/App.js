83693396213b1747d72b5bc5a0dfc97ea6884101
GustavoLisboa0 <gustavo20031010@gmail.com>
2026-03-02 19:29:40 -0300

    Add package.json for app_text_01 with initial dependencies and scripts

---
+++ app_Text_01/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+function App() {
+  return (
+    <View style={{flex: 1, padding: 30, backgroundColor: "#6d6d6d"}}>
+
+      <Text style={{backgroundColor: "#ff6600", fontSize: 24, textAlign: "center"}}>
+        TESTE COM O COMPONENTE TEXT
+      </Text>
+
+    </View>
+  );
+}
+
+export default App; 
