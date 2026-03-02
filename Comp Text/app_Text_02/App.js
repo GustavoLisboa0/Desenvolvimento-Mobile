6086a0456edce50d38001d21f3e8a53d093eb8c4
Bruno Carraro <70044423+carrarook@users.noreply.github.com>
2026-03-02 20:14:10 -0300

    Desafio 2

--- Comp Text/app_Text_02/App.js
+++ Comp Text/app_Text_02/App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import {View, Text} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  function App() {
+    return (
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      // BOX ONE
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+      //BOX TWO (ROW
+        <View style={{width: 300, height: 300, backgroundColor: 'black', flexDirection: 'row', flexWrap: 'wrap'}}>
+
+              <View style={{ width: '50%', height: '50%', backgroundColor: 'yellow' }}s>
+                  <Text style={{fontWeight: 'bold'}}> Texto 1 lalala 1234</Text>
+                  <Text style={{fontWeight: 'semibold'}}> Texto 1 lalala 1234</Text>
+                  </View>
+              <View style={{ width: '50%', height: '50%', backgroundColor: 'green' }}>
+                  <Text style={{color: 'blue'}}> Texto 2 lalala 1234</Text>
+                  <Text style={{color: 'orange'}}> Texto 2 lalala 1234</Text>
+
+                  </View>
+              <View style={{ width: '50%', height: '50%', backgroundColor: 'red' }}>
+                  <Text style={{fontSize: '30px'}}> Texto 3 lalala 1234</Text>
+                  </View>
+              <View style={{ width: '50%', height: '50%', backgroundColor: 'blue' }}>
+                  <Text> Texto 4 lalala 1234</Text>
+              </View>
+        </View>
+      </View>
+
+
+  ) }
+
+  export default App;
